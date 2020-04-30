71092647a2abebc4ccded8fcd324c739f6db049d
Aaron Contreras <aaronlcaq@gmail.com>
2020-04-29 21:00:21 -0500

    Fix power operator not splitting correctly

--- scripts/script.js
+++ scripts/script.js
@@ -30,108 +30,104 @@ function operate(a, operator, b) {
     return divide(a, b);
   }
 }
-const operation = {
-  operands: [],
-  operators: []
-};
+const nonOperators = document.querySelectorAll('.non-operator');
+nonOperators.forEach(button => {
+  button.setAttribute('data-symbol', button.textContent);
+});
 
-let currentOperand = '';
-let justClosed = false;
+let operation = '';
 let openedParensNum = 0;
 let closedParensNum = 0;
-const listOfOperations = [operation];
-let indexOfSubOject = 0;
 function updateDisplay(event) {
-  const button = event.target;
-  let current = listOfOperations.length - 1;
-  if (button.className === 'open-parens') {
-    newOperation = { operands: [], operators: [] };
-    listOfOperations[current].operands.push(newOperation);
-    listOfOperations.push(newOperation);
-  }
-  if (button.className === 'close-parens') {
-    listOfOperations[current].operands.push(+currentOperand);
-    listOfOperations.pop();
-    currentOperand = '';
-    current--;
-    justClosed = true;
-  }
-  if (button.className == 'digit') {
-    currentOperand += button.textContent;
-  } else if (event.target.className === 'operator') {
-    if (!justClosed) {
-
-      listOfOperations[current].operands.push(+currentOperand);
+  if (event.target.nodeName.toLowerCase() === 'button') {
+    if (event.target.className === 'equals') {
+      display.textContent = calculateResult(operation);
+      return;
     }
-    currentOperand = '';
-    listOfOperations[current].operators.push(button.getAttribute('data-symbol'));
-    justClosed = false;
-  } else if (event.target.className === 'equals') {
-    if (!justClosed) {
-
-      listOfOperations[current].operands.push(+currentOperand);
+    if (event.target.id === 'delete') {
+      operation = operation.slice(0, operation.length - 1);
+      console.log(operation);
+      display.textContent = operation;
+      return;
     }
-    currentOperand = '';
-    display.textContent = solve();
 
-    console.table(listOfOperations);
-    return;
+    operation += event.target.getAttribute('data-symbol');
+    display.textContent = operation;
   }
-
-  if (blankState) {
-    display.textContent = button.textContent;
-    blankState = false;
-  } else {
-    display.textContent += button.textContent;
-  }
-  console.table(listOfOperations);
 }
 
 function missingParentheses(openParentheses, closeParentheses) {
   return openParentheses === -1 && closeParentheses !== -1 ||
-      openParentheses !== -1 && closeParentheses === -1;
+    openParentheses !== -1 && closeParentheses === -1;
 }
 
-function noParentheses(openParentheses, closeParentheses) {
+function isLastOperation(openParentheses, closeParentheses) {
   return openParentheses === -1 && closeParentheses === -1;
 }
 
-function splitOperations(string) {
-  const parentheses = /\([d]*|[d]*\)/g;
-  const arr = string.split('');
-  const operatorList = ['(', ')', '**', '*', '/', '+', '-'];
-  const isOperatorOrOperation = element => operatorList.includes(element);
-  while (arr.some(isOperatorOrOperation)) {
-    const lastOpenParentheses = arr.lastIndexOf('(');
-    const firstCloseParentheses = arr.indexOf(')');
-    if (missingParentheses(lastOpenParentheses, firstCloseParentheses)) {
-      return "Fix unmatching brackets";
+const cleanOperation = function (string) {
+  const numbers = /([\d]*[.]{0,1}[\d]+)/g;
+  const power = /\*{2}/g;
+  const operator = /\D{2,}/g;
+  const operationElements = string.split(numbers);
+  for (let i = operationElements.length - 1; i >= 0; i--) {
+    const element = operationElements[i];
+    if (element.match(power)) {
+      operationElements.splice(i, 1, ...element.split(/(\*{2})/g));
+      continue;
     }
-    const distance = firstCloseParentheses - lastOpenParentheses + 1;
-    let listOfElementsToSolve = arr.slice(
-      lastOpenParentheses + 1,
-      firstCloseParentheses);
-      if (noParentheses(lastOpenParentheses, firstCloseParentheses)) {
-        listOfElementsToSolve = arr;
-      }
-    const operation = {
-      operands: listOfElementsToSolve
-        .filter(element => +element === +element),
-      operators: listOfElementsToSolve
-        .filter(element => isNaN(+element))
-    };
-    if (noParentheses(lastOpenParentheses, firstCloseParentheses)) {
-      return multiplyAndDivide(operation);
+    if (element.match(operator)) {
+      operationElements.splice(i, 1, ...element.split(''));
     }
-    console.log(operation);
-    arr.splice(lastOpenParentheses, distance, multiplyAndDivide(operation));
-    
-    console.log(arr);
   }
-  return arr;
+
+  return operationElements.filter(element => element !== '');
 }
 
-function multiplyAndDivide(operation) {
+function buildOperation(elements) {
+  return {
+    operands: elements.filter(element => +element === +element),
+    operators: elements.filter(element => isNaN(+element))
+  }
+}
+const nonOperands = ['(', ')',
+  '**', '*',
+  '/', '+',
+  '-'
+];
+
+const isNonOperand = element => nonOperands.includes(element);
+
+function calculateResult(stringOperation) {
+  const operation = cleanOperation(stringOperation);
+  console.log(operation);
+  while (operation.some(isNonOperand)) {
+    // const operationDelimiter = {
+    //   lastOpenParens: operation.lastIndexOf('('),
+    //   firstCloseParens: operation.indexOf(')'),
+    //   distance: lastOpenParens - firstCloseParens
+    // }
+    // console.log(operationDelimiter);
+    const lastOpenParens = operation.lastIndexOf('(');
+    const firstCloseParens = operation.indexOf(')');
+    const distance = firstCloseParens - lastOpenParens + 1;
+    if (missingParentheses(lastOpenParens, firstCloseParens)) {
+      return "Fix unmatching brackets";
+    }
+    if (isLastOperation(lastOpenParens, firstCloseParens)) {
+      return solve(buildOperation(operation));
+    }
+    const elementsToSolve = operation
+      .slice(lastOpenParens + 1, firstCloseParens);
+
+    const subOperation = buildOperation(elementsToSolve);
+    const solution = solve(subOperation);
+    operation.splice(lastOpenParens, distance, solution);
+  }
+  return operation;
+}
+
+function solve(operation) {
   const pendingOperators = operator => {
     return operator === '**' || operator === '*' || operator === '/' ||
       operator === '+' || operator === '-';
@@ -141,44 +137,29 @@ function multiplyAndDivide(operation) {
     ['*', '/'],
     ['+', '-']
   ];
-  const getOperatorsInOrder = () => {
+  const getHighestPriorityOperator = () => {
     let index = 0;
     for (let i = 0; i < operatorPriority.length; i++) {
       index = operation.operators.findIndex(operator => {
         return operatorPriority[i].includes(operator);
       });
-      if (index >= 0) {
-        return index;
-      }
+      if (index >= 0) return index;
     }
   }
 
   while (operation.operators.some(pendingOperators)) {
-    let index = getOperatorsInOrder();
-    console.log(index);
+    let index = getHighestPriorityOperator();
     const operator = operation.operators[index];
     const a = operation.operands[index];
     const b = operation.operands[index + 1];
     const result = operate(a, operator, b);
-
     operation.operands.splice(index, 2, result);
     operation.operators.splice(index, 1);
   }
-  console.log(operation.operands[0]);
   return operation.operands[0];
-}
-function solve() {
-  const result = multiplyAndDivide(operation);
-  console.log(result);
-  return result;
 }
 
 const display = document.querySelector('.display p');
 let blankState = true;
 const calculator = document.querySelector('#calc-container');
 calculator.addEventListener('click', updateDisplay);
-function getDisplayContent(display) {
-  return display.textContent;
-}
-const openParens = document.querySelector('.openParens');
-const closeParens = document.querySelector('.closeParens');
